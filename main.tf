aa707f273712c2d434aac49f119573ffb743b8af
ian-hancock <46765905+ian-hancock@users.noreply.github.com>
2021-04-22 08:52:12 +1000

    initial

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # Makes sure that we are using a pinned version of the provider
 
 terraform {
-  required_version = "~>0.12"
+  required_version = "~>0.13"
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2"
+      version = "~> 3"
     }
 } 
 
