3b02451fc5e17484a1ba857caed207c1c0c68bc6
ian-hancock <46765905+ian-hancock@users.noreply.github.com>
2021-04-22 08:47:19 +1000

    initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+# Makes sure that we are using a pinned version of the provider
+
+terraform {
+  required_version = "~>0.12"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2"
+    }
+} 
+
+#Gets the region from the calling provider and outputs it
+
+data "aws_region" "current" {}
+
+output currentregion {
+    value = data.aws_region.current.name
+}
